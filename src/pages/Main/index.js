50038aceb0da4f3400697b191f94612748de807f
Patrick Lima <patrickhl94@hotmail.com>
2020-04-08 18:48:04 -0300

    Updated index.js Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '../../services/api';
 import {Container, Input, Form, SubmitButton} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adcionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddSubmit = async () => {
+    const {newUser, users} = this.state;
+
+    const response = await api.get(`users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    console.tron.log(data);
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adcionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddSubmit}
+          />
+          <SubmitButton onPress={this.handleAddSubmit}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }

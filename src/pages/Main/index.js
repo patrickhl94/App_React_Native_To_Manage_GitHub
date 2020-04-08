ef9ebc97d50b4bcd43248bf9e96b50092867a3d0
Patrick Lima <patrickhl94@hotmail.com>
2020-04-08 01:00:14 -0300

    Updated index.js Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Input, Form, SubmitButton} from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <Text>Menu</Text>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adcionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
